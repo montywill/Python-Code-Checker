ccf899363b54dfd3ece2386476deabf3ef0675a5
montywill <montywill1025@gmail.com>
2026-02-17 14:21:12 -0500

    Add example file with lint issues for testing

---
+++ example_bad_script.py
@@ -0,0 +1,9 @@
+import os,sys   
+
+def foo():
+    x = 1    
+    y = 2
+    if x==1:
+        print("hello world this is a very very very very very very very very very very very long line that should trigger warning")
+    # TODO fix this later
+    return x+y
